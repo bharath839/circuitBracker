faa8420e0ac79d2b4668d5dd994938192b24fe0e
bharath839 <9553483839gb@gmail.com>
2024-12-29 19:00:35 +0530

    bharath

---
+++ src/main/java/com/circuitBracker/CircuitBrackerApplication.java
@@ -0,0 +1,13 @@
+package com.circuitBracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircuitBrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CircuitBrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/circuitBracker/CircuitBrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.circuitBracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircuitBrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
